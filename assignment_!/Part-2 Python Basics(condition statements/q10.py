f0951adfd837b09ae72938fd8c42a13f5f680610
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-03-18 22:27:30 +0500

    10 questions done

---
+++ assignment_!/Part-2 Python Basics(condition statements/q10.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter first number: "))  
+num2 = int(input("enter second number: "))  
+
+if num1 > num2:  
+    print("the first number is greater.")  
+elif num2 > num1:  
+    print("the second number is greater.")  
+else:  
+    print("both numbers are equal.")  
---
+++ assignment_!/Part-2 Python Basics(condition statements/q9.py
@@ -0,0 +1,6 @@
+length = int(input("Enter the length: "))
+breadth = int(input("Enter the breadth: "))
+if length == breadth:
+    print("It is a Square.")
+else:
+    print("It is a Rectangle.")
